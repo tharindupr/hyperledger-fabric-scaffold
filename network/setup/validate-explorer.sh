8dc07c058e05f127543d9cd5eb34dc782c45e3ef
tharindupr <tharindu.prf@gmail.com>
2021-09-20 16:58:19 +0100

    removed unnecessary files

--- network/setup/validate-explorer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "======Image for explorer ====="
-docker images | grep hlf-explorer
-
-echo "======Image for postgres ====="
-docker images | grep postgres
--- network/setup/validate-fabric.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "============ Peer Version =========="
-peer version
-echo "============ Orderer Version =========="
-orderer version
-echo "============ Fabric Docker Images =========="
-docker images
-
